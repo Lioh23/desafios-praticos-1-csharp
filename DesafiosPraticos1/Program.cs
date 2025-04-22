daed1ffa19c64454596a7a13e1d3d77863b94445
Aurélio Silva de Jesus <aurelin95@gmail.com>
2025-04-21 22:09:30 -0300

    refact list of prompt options in the main method

--- DesafiosPraticos1/Program.cs
+++ DesafiosPraticos1/Program.cs
@@ -9,14 +9,32 @@ class Program
     {
         var promptOptions = new List<PromptOption>
         {
-            new PromptOption(new MensagemBoasVindasChallenge(), "Programa em que o usuário precisa digitar um nome e uma mensagem de boas vindas"),
-            new PromptOption(new NomeESobrenomeChallenge(), "Programa que concatene um nome e um sobrenome inseridos pelo usuário final"),
-            new PromptOption(new CalculosMatematicosChallenge(), "Programa que receba 2 valores do tipo double e realiza cálculos matemáticos"),
-            new PromptOption(new ContagemCaracteresFraseChallenge(), "Programa que conta a quanitdade de caracteres da frase inserida"),
-            new PromptOption(new ValidacaoPlacaVeiculoChallenge(), "Programa que valida a placa de um veículo inserida"),
-            new PromptOption(new DataAtualDiferentesFormatosChallenge(), "Programa que solicita ao usuário a exibição da data atual em diferentes formatos")
+            new PromptOption(
+                label: "Programa em que o usuário precisa digitar um nome e uma mensagem de boas vindas",
+                executableObject: new MensagemBoasVindasChallenge()
+            ),
+            new PromptOption(
+                label: "Programa que concatene um nome e um sobrenome inseridos pelo usuário final",
+                executableObject: new NomeESobrenomeChallenge()
+            ),
+            new PromptOption(
+                label: "Programa que receba 2 valores do tipo double e realiza cálculos matemáticos",
+                executableObject: new CalculosMatematicosChallenge()
+            ),
+            new PromptOption(
+                label: "Programa que conta a quanitdade de caracteres da frase inserida",
+                executableObject: new ContagemCaracteresFraseChallenge()
+            ),
+            new PromptOption(
+                label: "Programa que valida a placa de um veículo inserida",
+                executableObject: new ValidacaoPlacaVeiculoChallenge()
+            ),
+            new PromptOption(
+                label: "Programa que solicita ao usuário a exibição da data atual em diferentes formatos",
+                executableObject: new DataAtualDiferentesFormatosChallenge()
+            )
         };
-        
+
         PromptHandler.Execute("Selecione o desafio que deseja testar pressionando \"ENTER\"", promptOptions);
     }
 }

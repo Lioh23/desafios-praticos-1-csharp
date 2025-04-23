06dce5710745b7a82eede33fff65ee5549d05e98
Aurélio Silva de Jesus <aurelin95@gmail.com>
2025-04-22 22:34:21 -0300

    feat: Conclusão desafio 5, validação de placas de veículos

--- DesafiosPraticos1/Desafios/ValidacaoPlacaVeiculoChallenge.cs
+++ DesafiosPraticos1/Desafios/ValidacaoPlacaVeiculoChallenge.cs
@@ -6,6 +6,36 @@ public class ValidacaoPlacaVeiculoChallenge : IExecutableInterface
 {
     public void Execute()
     {
-        Console.WriteLine("TODO");
+        Console.WriteLine("Insira a placa do veículo aqui");
+        string placa = Console.ReadLine();
+
+        string output =  HandleValidatePlaca(placa) ? "Verdadeiro " : "Falso";
+
+        Console.WriteLine(output);
+    }
+
+    static bool HandleValidatePlaca(string placa)
+    {
+        // valida quantidade de caracteres
+        if (placa.Length != 7)
+            return false;
+
+        int typeInteger;
+
+        // valida se os 3 primeiros caracteres são letras
+        for (int i = 0; i <= 2; i++)
+        {
+            if (int.TryParse(placa[i].ToString(), out typeInteger))
+                return false;
+        }
+
+        // valida se os 4 ultimos caracteres são números
+        for (int i = 3; i < placa.Length; i++)
+        {
+            if (!int.TryParse(placa[i].ToString(), out typeInteger))
+                return false;
+        }
+
+        return true;
     }
 }
